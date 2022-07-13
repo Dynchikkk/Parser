a1b8ddefd457c067b524521bdbf7a33acf00e8b8
Daniil Belyaev <dandombra1@gmail.com>
2022-07-13 20:39:28 +0300

    Навел марафет

--- FirstParcer.py
+++ FirstParcer.py
@@ -8,7 +8,7 @@ def return_soup_obj(link):
     return soup
 
 
-def vtuz_pars(link):
+def vtuz_pars(link, snils):
     soup = return_soup_obj(link)
 
     name_of_fac = soup.find("div", class_="container declaration-table").find("h2", class_="title").text.strip()
@@ -18,20 +18,16 @@ def vtuz_pars(link):
 
     for i in all_abit:
         all_inf = i.find_all("td")
-        abit_num = all_inf[0].text.strip()
         abit_snils = all_inf[1].text.strip()
-        if abit_snils == "148-061-749 63":
+        if abit_snils == snils:
+            abit_num = all_inf[0].text.strip()
             return [abit_num, abit_snils, name_of_fac]
 
-    return "Not in list"
+    return ["Not in list"]
 
 
-def pgu_pars(link):
+def pgu_pars(link, snils):
     page = 1
-
-    local_link = link + "p/" + str(page)
-    name_of_fac1 = return_soup_obj(local_link).find("div", class_="content-page")
-    name_of_fac_fin = name_of_fac1.find("a", href="/apply/list/faculty/").text.strip()
 
     while True:
         local_link = link + "p/" + str(page)
@@ -46,23 +42,59 @@ def pgu_pars(link):
 
         for i in all_abit:
             all_inf = i.find_all("td")
-            abit_num = all_inf[0].text.strip()
             abit_snils = all_inf[1].find("a").text.strip()
-            if abit_snils == "148-061-749 68":
+            if abit_snils == snils:
+                abit_num = all_inf[0].text.strip()
+                name_of_fac_fin = all_inf[2].find("a").text.strip()
                 return [abit_num, abit_snils, name_of_fac_fin]
 
         page += 1
-    return "Not in list"
+    return ["Not in list"]
 
 
-# VTUZ
-# http://abitur.penzgtu.ru/ru/entrants/09.03.01/665/ - inf and vyt texn
-# http://abitur.penzgtu.ru/ru/entrants/09.03.02/668/ - inf syst and texn
+def ask_parcer(vuz, link, snils):
+    # 1 - pgu
+    # 2 - vtuz
+    if vuz == 1:
+        return pgu_pars(link, snils)
+    if vuz == 2:
+        return vtuz_pars(link, snils)
+    return ["Not right vuz"]
 
-# PGU
-# https://www.pnzgu.ru/apply/list/faculty/31429808/speciality/1995/edu_level/2/edu_form/1/edu_quote1/1/edu_base/1/sort_field/name/sort_type/asc/
 
-link = input("Вставьте сслыку: ")
-print(*vtuz_pars(link))
+st_snils = "148-061-749 68"
+st_pgu_link = ["https://www.pnzgu.ru/apply/list/faculty/31429808/speciality/1995/edu_level/2/edu_form/1/edu_quote1/1"
+               "/edu_base/1/sort_field/name/sort_type/asc/",
+               "https://www.pnzgu.ru/apply/list/faculty/31429808/speciality/2006/edu_level/2/edu_form/1/edu_quote1/1"
+               "/edu_base/1/sort_field/name/sort_type/asc/",
+               "https://www.pnzgu.ru/apply/list/faculty/31429808/speciality/2012/edu_level/2/edu_form/1/edu_quote1/1"
+               "/edu_base/1/sort_field/name/sort_type/asc/"]
+st_vtuz_link = ["http://abitur.penzgtu.ru/ru/entrants/09.03.01/665/",
+                "http://abitur.penzgtu.ru/ru/entrants/09.03.02/668/",
+                "http://abitur.penzgtu.ru/ru/entrants/09.03.04/674/",
+                "http://abitur.penzgtu.ru/ru/entrants/09.03.03/671/"]
+
+while True:
+    step = int(input("1 - вставить ссылку, 2 - стандартные ссылки, 0 - выход: "))
+    if step == 0:
+        break
+    elif step == 1:
+        vuz = int(input("1 - ПГУ, 2 - ВТУЗ: "))
+        link = input("Вставьте сслыку: ")
+        snils = input("Вставьте снилс: ")
+        print("\n" + ", ".join(ask_parcer(vuz, link, snils)) + "\n")
+    elif step == 2:
+        print("ПГУ")
+        for i in st_pgu_link:
+            print(", ".join(ask_parcer(1, i, st_snils)))
+        print()
+        print("ВТУЗ")
+        for i in st_vtuz_link:
+            print(", ".join(ask_parcer(2, i, st_snils)))
+        print()
+
+
+# link = input("Вставьте сслыку: ")
+# print(*vtuz_pars(link))
 # print(pgu_pars(link))
 
